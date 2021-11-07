210c29ce3a927175c89da972bc202e10ef8a1ac5
brurya-e <brurya.zak@gmail.com>
2021-11-07 13:38:06 +0200

    2.1

---
+++ 2.1.js
@@ -0,0 +1,17 @@
+function describeCountry(country, population, capitalCity)
+{
+    return(`${country} has ${population} million people and its capital city is ${capitalCity}`)
+
+}
+
+let Finland = describeCountry('Finland', 6, 'Helsinki');
+let Israel = describeCountry('Israel', 9, 'Jerusalem');
+let France = describeCountry('France', 67.413, 'Paris');
+
+console.log(Finland);
+console.log(Israel);
+console.log(France);
+
+
+
+
