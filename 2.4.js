74df2d76abe76c35371426c6dc6b596825f838f9
brurya-e <brurya.zak@gmail.com>
2021-11-07 15:59:34 +0200

    short way

--- 2.4.js
+++ 2.4.js
@@ -1,30 +1,33 @@
 function someDetailsAbouteTheDay(today)
 {
-    const weekdays = new Array(7);
-        weekdays[0] = "Sunday";
-        weekdays[1] = "Monday";
-        weekdays[2] = "Tuesday";
-        weekdays[3] = "Wednesday";
-        weekdays[4] = "Thursday";
-        weekdays[5] = "Friday";
-        weekdays[6] = "Saturday";
-    let nameDay = weekdays[today.getDay()] ;
+    // const weekdays = new Array(7);
+    //     weekdays[0] = "Sunday";
+    //     weekdays[1] = "Monday";
+    //     weekdays[2] = "Tuesday";
+    //     weekdays[3] = "Wednesday";
+    //     weekdays[4] = "Thursday";
+    //     weekdays[5] = "Friday";
+    //     weekdays[6] = "Saturday";
+    // let nameDay = weekdays[today.getDay()] ;
+    let nameDay = today.toLocaleDateString('en-US',{ weekday: 'long' });  
 
-    const monthName = new Array();
-    monthName[0] = "January";
-    monthName[1] = "February";
-    monthName[2] = "March";
-    monthName[3] = "April";
-    monthName[4] = "May";
-    monthName[5] = "June";
-    monthName[6] = "July";
-    monthName[7] = "August";
-    monthName[8] = "September";
-    monthName[9] = "October";
-    monthName[10] = "November";
-    monthName[11] = "December";
-    let month = monthName[today.getMonth()];
+    // const monthName = new Array();
+    // monthName[0] = "January";
+    // monthName[1] = "February";
+    // monthName[2] = "March";
+    // monthName[3] = "April";
+    // monthName[4] = "May";
+    // monthName[5] = "June";
+    // monthName[6] = "July";
+    // monthName[7] = "August";
+    // monthName[8] = "September";
+    // monthName[9] = "October";
+    // monthName[10] = "November";
+    // monthName[11] = "December";
+    // let month = monthName[today.getMonth()];
+    let month = today.toLocaleDateString('en-US',{ month: 'long' });  
 
+    
     let dayInMonth = today.getDate();
     let year =today.getFullYear();
 
