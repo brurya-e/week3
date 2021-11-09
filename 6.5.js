0e6f10248591046808dc53e8115cd98f8e1c4c5d
brurya-e <brurya.zak@gmail.com>
2021-11-09 15:08:03 +0200

    6.5

--- 6.5.js
+++ 6.5.js
@@ -1,13 +1,13 @@
 // 1. Fill an array with 100 of a same object using array fill
 // method.
-let arr = Array(100).fill("hello");
+const arr = Array(100).fill("hello");
 
 // 2. Create an array with numbers ranging from 1-100 using
 // the Array.from method.
 
 // Since the array is initialized with `undefined` on each position,
 // the value of `v` below will be `undefined`
-let arr2 = Array.from({length: 100}, (v, i) => i+1);
+const arr2 = Array.from({length: 100}, (v, i) => i+1);
 
 // 3. Convert only values of an object into one array.
 function f() {
@@ -16,7 +16,7 @@ function f() {
 console.log( f('a',1,2,'b'));
 
 // 4. Convert an array into one object.
-arr3 = ['a','c', 'b'];
+const arr3 = ['a','c', 'b'];
 const convertArrayToObject = arr3.join('');
 console.log(convertArrayToObject);
 
@@ -28,13 +28,13 @@ console.log(Array.isArray(convertArrayToObject));
 // 6. Copy an array.
 // • Create a copy of an array that won’t effect the original
 // array if modified.
- let arr4 = [1,2,3,4];
- let arr5 = arr4;
+ const arr4 = [1,2,3,4];
+ const arr5 = arr4;
  arr5[2]= 5;
  //effect on both array
 
 // • Create a copy of an array that will effect the original
 // array if modified.
 
-//use slice, spice or contact
-let newArray = arr4.slice();
+//use slice, spice or conact
+const newArray = arr4.slice();
